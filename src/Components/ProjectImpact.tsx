0774f80b6f80c6e5b0010a0339939bbc37ad667d
10sandeep <sandeepnayak1424@gmail.com>
2025-08-20 12:22:07 +0530

    small changed

--- src/Components/ProjectImpact.tsx
+++ src/Components/ProjectImpact.tsx
@@ -18,7 +18,7 @@ const ProjectImpact: React.FC = () => {
     },
     {
       icon: <MapPin className="w-8 h-8" />,
-      value: "1",
+      value: "2",
       label: "Month Program",
     },
     {
